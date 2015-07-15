b6b96452e804ae43704ecbb7a5eb457b85f8cc7b
IanConery <ianconery@gmail.com>
2015-07-15 16:07:20 -0700

    got the song to play

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,15 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
+  model: SongModel,
 
   initialize: function(){
-  }
+  },
 
+  events: {
+    'enqueue:model': 'addSong'
+  },
+
+  addSong: function() {
+    this.collection.add(this.model);
+  }
 });
